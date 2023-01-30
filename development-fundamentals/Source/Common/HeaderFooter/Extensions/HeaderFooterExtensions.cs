c284d23f1e11bb383961a34518efa7eeba6eac34
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-30 14:04:06 +0530

    Initial Version

---
+++ development-fundamentals/Source/Common/HeaderFooter/Extensions/HeaderFooterExtensions.cs
@@ -0,0 +1,21 @@
+﻿using HeaderFooter.Lib.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HeaderFooter.Lib.Extensions
+{
+
+    public static class HeaderFooterExtensions
+    {
+
+        public static IServiceCollection ConfigureHeaderFooterServices(this IServiceCollection services)
+        {
+            _ = services.AddTransient<IFooter, Footer>();
+
+            _ = services.AddTransient<IHeader, Header>();
+
+            return services;
+        }
+
+    }
+
+}
---
+++ development-fundamentals/Source/Common/NamesGenerator/Extensions/NamesGeneratorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NamesGenerator.Lib.Interfaces;
+
+namespace NamesGenerator.Lib.Extensions
+{
+    public static class NamesGeneratorExtensions
+    {
+        public static IServiceCollection ConfigureNamesGeneratorServices(this IServiceCollection services)
+        {
+            _ = services.AddTransient<IGenerateNames, GenerateNames>();
+
+            return services;
+        }
+    }
+
+}
