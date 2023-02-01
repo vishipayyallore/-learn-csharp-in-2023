690e00e09e8429c7fba2936ab6f5dbfeaa546c87
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-01 13:13:13 +0530

    Generating Names

---
+++ development-fundamentals/Source/ArrayDemos/NamesArray.Lib/NamesArray.cs
@@ -0,0 +1,23 @@
+﻿using NamesArray.Lib.Interfaces;
+using NamesGenerator.Interfaces;
+
+namespace NamesArray.Lib
+{
+
+    public class NamesArray : INamesArray
+    {
+        private readonly IGenerateNames _nameGenerator;
+
+        public NamesArray(IGenerateNames nameGenerator)
+        {
+            _nameGenerator = nameGenerator ?? throw new ArgumentNullException(nameof(nameGenerator));
+        }
+
+        public string[] GenerateNames(int nameLength = 10, int numberOfNames = 10)
+        {
+            return _nameGenerator.GetNames(nameLength, numberOfNames);
+        }
+
+    }
+
+}
