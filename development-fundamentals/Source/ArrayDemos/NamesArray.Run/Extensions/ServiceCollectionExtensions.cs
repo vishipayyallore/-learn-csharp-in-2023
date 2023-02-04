cbec7dd6ef510c62c9d51369c7d7dde2d68d4494
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-04 17:13:34 +0530

    Initial Version

---
+++ development-fundamentals/Source/ArrayDemos/NamesArray.Run/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using HeaderFooter.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using NamesArray.Lib.Extensions;
+using NamesGenerator.Extensions;
+
+namespace NamesArray.Run.Extensions
+{
+
+    public static class ServiceCollectionExtensions
+    {
+        public static ServiceProvider ConfigureServices()
+        {
+            // create service collection
+            IServiceCollection services = new ServiceCollection();
+
+            _ = services.ConfigureHeaderFooterServices();
+            _ = services.ConfigureNamesGeneratorServices();
+            _ = services.ConfigureNamesArrayDemoServices();
+
+            // IMPORTANT! Register the application entry point
+            // _ = services.AddTransient<NamesArrayDemoApp>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
