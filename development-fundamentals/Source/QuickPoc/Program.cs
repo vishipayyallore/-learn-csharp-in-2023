ef908fa191182f0cde9d04807bc6007ead0d1ace
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-07 07:12:29 +0530

    Initial Program

--- development-fundamentals/Source/QuickPoc/Program.cs
+++ development-fundamentals/Source/QuickPoc/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using QuickPoc.Set1;
+
+SetOneDemos.Run();
+
+Console.WriteLine("\nPress any key ...");
