d3bcd9ed72e595ee5d9fdc117c6e689bc51d1261
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-08 13:18:32 +0530

    Command Line Arguments

--- development-fundamentals/Source/QuickPoc/Program.cs
+++ development-fundamentals/Source/QuickPoc/Program.cs
@@ -12,4 +12,44 @@ using var host = Host.CreateDefaultBuilder(args)
 
 host.Services.GetRequiredService<SetOneRunner>()?.Run();
 
+ShowCommandLineArguments(args);
+
 Console.WriteLine("\nPress any key ...");
+
+static void ShowCommandLineArguments(string[] args)
+{
+    WriteLine($"There are {args.Length} arguments.");
+
+    if (args.Length < 3)
+    {
+        WriteLine("You must specify two colors and cursor size, e.g.");
+        WriteLine("dotnet run red yellow 50");
+        return; // stop running
+    }
+
+    ForegroundColor = (ConsoleColor)Enum.Parse(
+      enumType: typeof(ConsoleColor),
+      value: args[0],
+      ignoreCase: true);
+
+    BackgroundColor = (ConsoleColor)Enum.Parse(
+      enumType: typeof(ConsoleColor),
+      value: args[1],
+      ignoreCase: true);
+
+    try
+    {
+        CursorSize = int.Parse(args[2]);
+    }
+    catch (PlatformNotSupportedException)
+    {
+        WriteLine("The current platform does not support changing the size of the cursor.");
+    }
+
+    foreach (string arg in args)
+    {
+        WriteLine(arg);
+    }
+
+    ResetColor();
+}
---
+++ development-fundamentals/Source/QuickPoc/Set1/CommandLineArguments.cs
@@ -0,0 +1,18 @@
+﻿using QuickPoc.Interfaces;
+
+namespace QuickPoc.Set1
+{
+
+    internal class CommandLineArguments : IDemoProgram
+    {
+
+        public void Run()
+        {
+            // See https://aka.ms/new-console-template for more information
+            WriteLine(Environment.CurrentDirectory);
+            WriteLine(Environment.OSVersion.VersionString);
+        }
+
+    }
+
+}
