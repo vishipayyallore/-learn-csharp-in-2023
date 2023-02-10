647872ee0a5efcc9aa19f68b880839d5e958f9b3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-10 07:48:28 +0530

    Swamy/09feb work (#8)
    * Initial Version
    
    * Version 1
    
    * Update development-fundamentals.sln

---
+++ development-fundamentals/Source/Common/PrintHelperLib/Interfaces/IPrintHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace PrintHelperLib.Interfaces
+{
+
+    public interface IPrintHelper
+    {
+        void Print(string[] names);
+
+        void Print(ArrayList names);
+    }
+
+}
---
+++ development-fundamentals/Source/Common/PrintHelperLib/PrintHelper.cs
@@ -0,0 +1,28 @@
+﻿using PrintHelperLib.Interfaces;
+using System.Collections;
+
+namespace PrintHelperLib
+{
+
+    public class PrintHelper : IPrintHelper
+    {
+        public void Print(string[] names)
+        {
+            int index = 1;
+
+            Array.ForEach(names, (name) =>
+            {
+                WriteLine($"{index++}. {name} ({name.Length})");
+            });
+        }
+
+        public void Print(ArrayList names)
+        {
+            foreach (string name in names)
+            {
+                WriteLine(name);
+            }
+        }
+    }
+
+}
