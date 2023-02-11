0b32ffe0b5892ffd62760800bfbd1553bb29e01d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-11 14:40:11 +0530

    Error Version

--- development-fundamentals/Source/QuickPoc/Program.cs
+++ development-fundamentals/Source/QuickPoc/Program.cs
@@ -4,6 +4,9 @@ using QuickPoc.Extensions;
 using QuickPoc.Runners;
 using QuickPoc.Set1;
 
+// Compiler version: '4.4.0-6.22608.27 (af1e46ad)'.Language version: 11.0.QuickPoc    C:\LordKrishna\GitHub\learn - csharp -in-2023\development - fundamentals\Source\QuickPoc\Program.cs  7   Active
+// #error version 
+
 using var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((_, services) =>
     {
