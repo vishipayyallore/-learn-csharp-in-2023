663e18e938309a953ed45bda3b4bc61038e76438
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-14 14:45:50 +0530

    Initial Version

---
+++ mini-projects/A1SimpleGame/Program.cs
@@ -0,0 +1,9 @@
+﻿
+WriteLine("Hello, what is your name?");
+
+var name = ReadLine() ?? "No Name";
+
+WriteLine("Welcome {0} to A1 Simple Game!", name);
+
+WriteLine("\nThank You. Press any key ...");
+ReadKey();
