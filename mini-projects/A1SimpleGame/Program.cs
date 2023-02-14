3e5de4d326a8499dafb649f04a39575c11e17d25
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-14 15:06:11 +0530

    Code Refactoring

--- mini-projects/A1SimpleGame/Program.cs
+++ mini-projects/A1SimpleGame/Program.cs
@@ -1,10 +1,14 @@
-﻿WriteLine("Hello, what is your name?");
+﻿using A1SimpleGame.src.Characters;
+
+WriteLine("Hello, what is your name?");
 
 var name = ReadLine();
 
 name = string.IsNullOrWhiteSpace(name) ? "No Name" : name;
 
-WriteLine("Welcome {0} to A1 Simple Game!", name);
+var player = new Player(name);
+
+WriteLine("Welcome {0} to A1 Simple Game!", player.Name);
 
 WriteLine("\nThank You. Press any key ...");
 ReadKey();
