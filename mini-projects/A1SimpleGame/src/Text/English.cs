3192d2e87c9d055d8a22db8127a1001d8fde0d1d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-17 23:49:53 +0530

    Creating Text

---
+++ mini-projects/A1SimpleGame/src/Text/English.cs
@@ -0,0 +1,15 @@
+﻿namespace A1SimpleGame.src.Text;
+
+public class English : Language
+{
+
+    public English()
+    {
+        Welcome = "Welcome {0} to your OOP Adventure!";
+
+        ChooseYourName = "Hello, what is your name?";
+
+        DefaultName = "No Name";
+    }
+
+}
