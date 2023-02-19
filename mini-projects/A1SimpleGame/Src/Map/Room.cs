92183b52ed755c07a848e80e46395cd69956fc55
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-19 19:23:52 +0530

    Create Room.cs

---
+++ mini-projects/A1SimpleGame/Src/Map/Room.cs
@@ -0,0 +1,26 @@
+﻿using A1SimpleGame.Src.Text;
+
+namespace A1SimpleGame.Src.Map;
+
+public class Room
+{
+    public string Name { get; set; } = SpecifyText.Language.DefaultRoomName;
+
+    public string Description { get; set; } = SpecifyText.Language.DefaultRoomDescription;
+
+    public bool Visited { get; set; }
+
+    public Dictionary<Directions, int> Neighbors { get; set; } = new()
+    {
+        {Directions.North, -1 },
+
+        {Directions.East, -1 },
+
+        {Directions.South, -1 },
+
+        {Directions.West, -1 },
+
+        {Directions.None, -1 },
+    };
+
+}
