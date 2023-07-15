c4d4c546380fb0602220b8fa077bc513a5151ae6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-15 22:06:17 +0530

    Update Program.cs

--- sample-algos/src/beginner/ArrayAlgosDemo/Program.cs
+++ sample-algos/src/beginner/ArrayAlgosDemo/Program.cs
@@ -1,18 +1,28 @@
 ﻿using ArrayAlgosDemo;
 
-TwoNumbersSum.ShowDemo();
+// TwoNumbersSum.ShowDemo();
 
-var results = IsValidSubsequence(new List<int> { 1, 2, 3, 4 }, new List<int> { 1, 3 });
+// var results = IsValidSubsequence(new List<int> { 1, 2, 3, 4 }, new List<int> { 1, 3 });
+// var results = IsValidSubsequence(new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 }, new List<int> { 1, 6, -1, 10 });
+var results = IsValidSubsequence(new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 }, new List<int> { 5, 1, 22, 22, 6, -1, 8, 10 });
 WriteLine(results);
 
-static bool IsValidSubsequence(List<int> inputNumbers, List<int> sequence)
+static bool IsValidSubsequence(List<int> array, List<int> sequence)
 {
+    if (sequence.Count > array.Count || (sequence.Count != sequence.ToHashSet().Count && sequence.ToHashSet().Count != 1))
+    {
+        return false;
+    }
+
+    var currentIndex = 0;
     foreach (var item in sequence)
     {
-        if (!inputNumbers.Contains(item))
+        var index = array.IndexOf(item);
+        if (index == -1 || index < currentIndex)
         {
             return false;
         }
+        currentIndex = index;
     }
 
     return true;
