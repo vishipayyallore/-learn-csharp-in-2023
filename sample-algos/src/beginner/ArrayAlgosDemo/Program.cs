2d30a360e7a5ccf5b625503c13eb8d732095e16f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-15 19:15:11 +0530

    Code Refactoring

--- sample-algos/src/beginner/ArrayAlgosDemo/Program.cs
+++ sample-algos/src/beginner/ArrayAlgosDemo/Program.cs
@@ -1,33 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿ShowTwoNumberSumDemo();
 
 
-ShowTwoNumberSumDemo();
 
-static void ShowTwoNumberSumDemo()
-{
-    ForegroundColor = ConsoleColor.DarkCyan;
-    int[] outputArray = TwoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10);
-    string[] strArray = Array.ConvertAll(outputArray, ele => ele.ToString());
-    WriteLine($"[{string.Join(", ", strArray)}]");
 
-    ResetColor();
-}
-
-static int[] TwoNumberSum(int[] inputNumbers, int sumToMatch)
-{
-    for (int i = 0; i < inputNumbers.Length - 1; i++)
-    {
-        for (int j = i + 1; j < inputNumbers.Length; j++)
-        {
-            // Uncomment this line for Debugging
-            // WriteLine($"{i + 1}. {inputNumbers[i]} + {inputNumbers[j]}");
-
-            if ((inputNumbers[i] + inputNumbers[j]) == sumToMatch)
-            {
-                return new[] { inputNumbers[i], inputNumbers[j] };
-            }
-        }
-    }
-
-    return Array.Empty<int>();
-}
---
+++ sample-algos/src/beginner/ArrayAlgosDemo/TwoNumbersSum.cs
@@ -0,0 +1,42 @@
+﻿using ArrayAlgosDemo.Common;
+
+namespace ArrayAlgosDemo;
+
+public static class TwoNumbersSum
+{
+    public static void ShowTwoNumberSumDemo()
+    {
+        ForegroundColor = ConsoleColor.DarkCyan;
+        WriteLine("***** Two Numbers Sum *****");
+        int[] outputArray = TwoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10);
+
+        WriteLine($"Output: [{string.Join(", ", Helpers.ConvertIntToStringArray(outputArray))}]");
+
+        WriteLine("----- Two Numbers Sum -----");
+        ResetColor();
+    }
+
+
+    private static int[] TwoNumberSum(int[] inputNumbers, int sumToMatch)
+    {
+        WriteLine($"Input Array: [{string.Join(", ", Helpers.ConvertIntToStringArray(inputNumbers))}]");
+        WriteLine($"Sum to Match: {sumToMatch}");
+
+        for (int i = 0; i < inputNumbers.Length - 1; i++)
+        {
+            for (int j = i + 1; j < inputNumbers.Length; j++)
+            {
+                // Uncomment this line for Debugging
+                // WriteLine($"{i + 1}. {inputNumbers[i]} + {inputNumbers[j]}");
+
+                if ((inputNumbers[i] + inputNumbers[j]) == sumToMatch)
+                {
+                    return new[] { inputNumbers[i], inputNumbers[j] };
+                }
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+
+}
