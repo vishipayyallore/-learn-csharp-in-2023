bfdf68b1788284701555914e7bc1502566bcfa34
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-15 19:35:29 +0530

    Update Program.cs

--- sample-algos/src/beginner/ArrayAlgosDemo/Program.cs
+++ sample-algos/src/beginner/ArrayAlgosDemo/Program.cs
@@ -1,3 +1,19 @@
 ﻿using ArrayAlgosDemo;
 
 TwoNumbersSum.ShowDemo();
+
+var results = IsValidSubsequence(new List<int> { 1, 2, 3, 4 }, new List<int> { 1, 3 });
+WriteLine(results);
+
+static bool IsValidSubsequence(List<int> inputNumbers, List<int> sequence)
+{
+    foreach (var item in sequence)
+    {
+        if (!inputNumbers.Contains(item))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
