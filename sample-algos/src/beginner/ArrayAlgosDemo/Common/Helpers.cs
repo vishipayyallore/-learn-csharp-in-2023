22e933e525dcf77d04842bcc76a300cd7b963f90
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-16 10:49:06 +0530

    Swamy/14jul algos day1 (#27)
    * Initial Version
    
    * Renamed the file
    
    * Verified the Algorithm
    
    * Folder Reorganization
    
    * Code Refactoring
    
    * Update Program.cs
    
    * Code Refactoring
    
    * Code Refactoring
    
    * Update Program.cs
    
    * Update Program.cs
    
    * Update Program.cs

---
+++ sample-algos/src/beginner/ArrayAlgosDemo/Common/Helpers.cs
@@ -0,0 +1,11 @@
+﻿namespace ArrayAlgosDemo.Common;
+
+public static class Helpers
+{
+
+    public static string[] ConvertIntToStringArray(int[] inputArray)
+    {
+        return Array.ConvertAll(inputArray, ele => ele.ToString());
+    }
+
+}
