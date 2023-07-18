b043b6e54afc93eeab580181b73ef8955cb6bba8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-18 21:56:48 +0530

    Update Program.cs

--- sample-algos/src/beginner/ListAlgosDemo/Program.cs
+++ sample-algos/src/beginner/ListAlgosDemo/Program.cs
@@ -1,4 +1,25 @@
 ﻿using ListAlgosDemo;
+using System.Data.Common;
 
-ValidateSubsequence.ShowDemo();
+ForegroundColor = ConsoleColor.DarkCyan;
+
+var color = "DarkCyan";
+Enum.TryParse<ConsoleColor>(color?.TrimEnd(), out ConsoleColor res);
+WriteLine($"Color: {res}");
+
+color = null;
+Enum.TryParse<ConsoleColor>(color?.TrimEnd(), out res);
+WriteLine($"Color: {res}");
+
+color = string.Empty;
+Enum.TryParse<ConsoleColor>(color?.TrimEnd(), out res);
+WriteLine($"Color: {res}");
+
+color = "";
+Enum.TryParse<ConsoleColor>(color?.TrimEnd(), out res);
+WriteLine($"Color: {res}");
+
+ResetColor();
+
+// ValidateSubsequence.ShowDemo();
 
