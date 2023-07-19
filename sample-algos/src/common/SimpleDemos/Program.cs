51308ce4bcb54f8ca958dec3352a8afcff1998ae
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-19 10:06:38 +0530

    Swamy/17jul work (#29)
    * Initial Version
    
    * Update Program.cs
    
    * Code Refactoring
    
    * Code Refactoring
    
    * Update Program.cs
    
    * Initial Version
    
    * Code Refactoring
    
    * Code Refactoring

---
+++ sample-algos/src/common/SimpleDemos/Program.cs
@@ -0,0 +1,25 @@
+﻿
+ShowCaseTryParseEnumDemo();
+
+static void ShowCaseTryParseEnumDemo()
+{
+    ForegroundColor = ConsoleColor.DarkCyan;
+
+    var color = "DarkCyan";
+    _ = Enum.TryParse(color?.TrimEnd(), out ConsoleColor res);
+    WriteLine($"Color: {res}");
+
+    color = null;
+    _ = Enum.TryParse(color?.TrimEnd(), out res);
+    WriteLine($"Color: {res}");
+
+    color = string.Empty;
+    _ = Enum.TryParse(color?.TrimEnd(), out res);
+    WriteLine($"Color: {res}");
+
+    color = "";
+    _ = Enum.TryParse(color?.TrimEnd(), out res);
+    WriteLine($"Color: {res}");
+
+    ResetColor();
+}
