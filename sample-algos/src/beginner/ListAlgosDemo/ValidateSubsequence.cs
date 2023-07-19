e6709a4392177da9ef14f061f0badf8799753d7e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-19 23:17:31 +0530

    Code Refactoring

--- sample-algos/src/beginner/ListAlgosDemo/ValidateSubsequence.cs
+++ sample-algos/src/beginner/ListAlgosDemo/ValidateSubsequence.cs
@@ -1,21 +1,29 @@
-﻿using ListAlgosDemo.Common;
+﻿using HeaderFooter;
+using HeaderFooter.Interfaces;
+using ListAlgosDemo.Common;
 
 namespace ListAlgosDemo;
 
 public static class ValidateSubsequence
 {
+
+    private static readonly IHeader _header = new Header();
+    private static readonly IFooter _footer = new Footer();
+
     public static void ShowDemo()
     {
-        ForegroundColor = ConsoleColor.Yellow;
-        WriteLine("***** Validate Subsequence *****");
+        _header.DisplayHeader('=', "Validate Subsequence");
+
+        ForegroundColor = ConsoleColor.DarkCyan;
 
         // var results = IsValidSubsequence(new List<int> { 1, 2, 3, 4 }, new List<int> { 1, 3 });
         var results = IsValidSubsequence(new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 }, new List<int> { 1, 6, -1, 10 });
         // var results = IsValidSubsequence(new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 }, new List<int> { 5, 1, 22, 22, 6, -1, 8, 10 });
         WriteLine($"Is Valid Subsequence: {results}");
 
-        WriteLine("----- Validate Subsequence -----");
         ResetColor();
+
+        _footer.DisplayFooter('-');
     }
 
     private static bool IsValidSubsequence(List<int> numbersList, List<int> sequence)
