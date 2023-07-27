bea08c5854e9e9bb665564fa2079eaf2bffe0fea
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-27 09:54:10 +0530

    Swamy/26jul algo practice (#31)
    * Update TwoNumbersSum.cs
    
    * Update TwoNumbersSum.cs
    
    * Update TwoNumbersSum.cs
    
    * Update TwoNumbersSum.cs

--- sample-algos/src/beginner/ArrayAlgosDemo/Alogs/TwoNumbersSum.cs
+++ sample-algos/src/beginner/ArrayAlgosDemo/Alogs/TwoNumbersSum.cs
@@ -7,14 +7,26 @@ public static class TwoNumbersSum
 {
     private static readonly Header _header = new();
     private static readonly Footer _footer = new();
+    private static readonly string[] titles = { "Two Numbers Sum - V1", "Two Numbers Sum - V2" };
 
     public static void ShowDemo()
     {
-        _header.DisplayHeader('=', "Two Numbers Sum");
+        var inputNumbers = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 };
+        var sumToMatch = 10;
 
-        ForegroundColor = ConsoleColor.DarkCyan;
+        ExecuteTwoNumberSum(TwoNumberSumV1, inputNumbers, sumToMatch, titles[0], ConsoleColor.DarkCyan);
 
-        int[] outputArray = TwoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10);
+        ExecuteTwoNumberSum(TwoNumberSumV2, inputNumbers, sumToMatch, titles[1], ConsoleColor.DarkYellow);
+    }
+
+    private static void ExecuteTwoNumberSum(Func<int[], int, int[]> twoNumberSumMethod,
+        int[] inputNumbers, int sumToMatch, string title, ConsoleColor consoleColor)
+    {
+        _header.DisplayHeader('=', title);
+
+        ForegroundColor = consoleColor;
+
+        int[] outputArray = twoNumberSumMethod(inputNumbers, sumToMatch);
 
         WriteLine($"Output: [{Helpers.ConvertIntArrayToString(outputArray)}]");
 
@@ -23,17 +35,14 @@ public static class TwoNumbersSum
         _footer.DisplayFooter('-');
     }
 
-    private static int[] TwoNumberSum(int[] inputNumbers, int sumToMatch)
+    private static int[] TwoNumberSumV1(int[] inputNumbers, int sumToMatch)
     {
-        WriteLine($"Input Array: [{Helpers.ConvertIntArrayToString(inputNumbers)}]");
-        WriteLine($"Sum to Match: {sumToMatch}");
+        DisplayInputAndSum(inputNumbers, sumToMatch); ;
 
         for (int i = 0; i < inputNumbers.Length - 1; i++)
         {
             for (int j = i + 1; j < inputNumbers.Length; j++)
             {
-                // WriteLine($"{i + 1}. {inputNumbers[i]} + {inputNumbers[j]}"); // Uncomment this line for Debugging
-
                 if (inputNumbers[i] + inputNumbers[j] == sumToMatch)
                 {
                     return new[] { inputNumbers[i], inputNumbers[j] };
@@ -43,4 +52,125 @@ public static class TwoNumbersSum
 
         return Array.Empty<int>();
     }
+
+    private static int[] TwoNumberSumV2(int[] inputNumbers, int sumToMatch)
+    {
+        DisplayInputAndSum(inputNumbers, sumToMatch);
+
+        HashSet<int> visited = new();
+
+        for (int i = 0; i < inputNumbers.Length; i++)
+        {
+            int value = sumToMatch - inputNumbers[i];
+
+            if (visited.Contains(value))
+            {
+                return new[] { inputNumbers[i], value };
+            }
+
+            visited.Add(inputNumbers[i]);
+        }
+
+        return Array.Empty<int>();
+    }
+
+    private static void DisplayInputAndSum(int[] inputNumbers, int sumToMatch)
+    {
+        WriteLine($"Input Array: [{Helpers.ConvertIntArrayToString(inputNumbers)}]");
+        WriteLine($"Sum to Match: {sumToMatch}");
+    }
+
 }
+
+
+//using System;
+//using System.Collections.Generic;
+
+//public static class TwoNumbersSum
+//{
+//    private static readonly Header _header = new();
+//    private static readonly Footer _footer = new();
+//    private static readonly string[] titles = { "Two Numbers Sum - V1", "Two Numbers Sum - V2" };
+
+//    public static void ShowDemo()
+//    {
+//        var inputNumbers = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 };
+//        var sumToMatch = 10;
+
+//        // Using Func<T> version for TwoNumberSumV1 logic
+//        ExecuteTwoNumberSum(inputNumbers, sumToMatch, TwoNumberSumV1, titles[0], ConsoleColor.DarkCyan);
+
+//        // Using Func<T> version for TwoNumberSumV2 logic
+//        ExecuteTwoNumberSum(inputNumbers, sumToMatch, TwoNumberSumV2, titles[1], ConsoleColor.DarkYellow);
+//    }
+
+//    private static void ExecuteTwoNumberSum(int[] inputNumbers, int sumToMatch, Func<int[], int, int[]> twoNumberSumFunc,
+//        string title, ConsoleColor consoleColor)
+//    {
+//        _header.DisplayHeader('=', title);
+
+//        Console.ForegroundColor = consoleColor;
+
+//        // Common Logic A: Display input and sum
+//        DisplayInputAndSum(inputNumbers, sumToMatch);
+
+//        // Execute the provided function (Func<T>)
+//        int[] outputArray = twoNumberSumFunc(inputNumbers, sumToMatch);
+
+//        // Common Logic B: Display output
+//        WriteLine($"Output: [{Helpers.ConvertIntArrayToString(outputArray)}]");
+
+//        Console.ResetColor();
+
+//        _footer.DisplayFooter('-');
+//    }
+
+//    private static int[] TwoNumberSumV1(int[] inputNumbers, int sumToMatch)
+//    {
+//        for (int i = 0; i < inputNumbers.Length - 1; i++)
+//        {
+//            for (int j = i + 1; j < inputNumbers.Length; j++)
+//            {
+//                if (inputNumbers[i] + inputNumbers[j] == sumToMatch)
+//                {
+//                    return new[] { inputNumbers[i], inputNumbers[j] };
+//                }
+//            }
+//        }
+
+//        // Common Logic B: Return Array.Empty<int>() when no match is found
+//        return Array.Empty<int>();
+//    }
+
+//    private static int[] TwoNumberSumV2(int[] inputNumbers, int sumToMatch)
+//    {
+//        HashSet<int> visited = new();
+
+//        for (int i = 0; i < inputNumbers.Length; i++)
+//        {
+//            int value = sumToMatch - inputNumbers[i];
+
+//            if (visited.Contains(value))
+//            {
+//                return new[] { inputNumbers[i], value };
+//            }
+
+//            visited.Add(inputNumbers[i]);
+//        }
+
+//        // Common Logic B: Return Array.Empty<int>() when no match is found
+//        return Array.Empty<int>();
+//    }
+
+//    private static void DisplayInputAndSum(int[] inputNumbers, int sumToMatch)
+//    {
+//        WriteLine($"Input Array: [{Helpers.ConvertIntArrayToString(inputNumbers)}]");
+//        WriteLine($"Sum to Match: {sumToMatch}");
+//    }
+
+//    // Helper method to avoid Console.WriteLine() duplication
+//    private static void WriteLine(string message)
+//    {
+//        Console.WriteLine(message);
+//    }
+//}
