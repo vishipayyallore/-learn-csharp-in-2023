7d52b89edf7f5ba16192d05d5fc12e8774b8da9b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-28 13:50:31 +0530

    Swamy/27jul algo practice (#33)
    * Code Refactoring
    
    * Code Refactoring
    
    * Code Refactoring
    
    * Update TwoNumbersSum.cs
    
    * Update TwoNumbersSum.cs
    
    * Add Colors
    
    * Changes

--- sample-algos/src/beginner/ArrayAlgosDemo/Common/ConsoleColorStyleHelper.cs
+++ sample-algos/src/beginner/ArrayAlgosDemo/Common/ConsoleColorStyleHelper.cs
@@ -4,6 +4,10 @@ public static class ConsoleColorStyleHelper
 {
     public static ConsoleColor GetConsoleForegroundColor(ConsoleColorStyle style) => style switch
     {
+        ConsoleColorStyle.DarkYellow => ConsoleColor.DarkYellow,
+
+        ConsoleColorStyle.DarkCyan => ConsoleColor.DarkCyan,
+
         ConsoleColorStyle.DarkGreen => ConsoleColor.DarkGreen,
 
         ConsoleColorStyle.DarkMagenta => ConsoleColor.DarkMagenta,
