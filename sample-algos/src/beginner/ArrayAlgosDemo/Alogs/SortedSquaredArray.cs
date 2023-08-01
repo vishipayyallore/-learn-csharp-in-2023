a688b2673ccbf06f08fa401bdf2f9923a901fb99
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-01 14:03:49 +0530

    Code

---
+++ sample-algos/src/beginner/ArrayAlgosDemo/Alogs/SortedSquaredArray.cs
@@ -0,0 +1,18 @@
+﻿using HeaderFooter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAlgosDemo.Alogs;
+
+public static class SortedSquaredArray
+{
+    private static readonly Header _header = new();
+    private static readonly Footer _footer = new();
+    private static readonly string[] titles = { "SortedSquaredArray" };
+
+
+
+}
