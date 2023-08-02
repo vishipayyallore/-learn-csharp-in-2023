079f4877b4c249282dd2c47104c9dbc1f7e741e4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-02 21:24:00 +0530

    Update Program.cs

--- sample-algos/src/quickpocs/SimpleDemos/Program.cs
+++ sample-algos/src/quickpocs/SimpleDemos/Program.cs
@@ -1,6 +1,42 @@
 ﻿
 ShowCaseTryParseEnumDemo();
 
+List<List<string>> competitions = new()
+{
+    new() { "HTML", "C#" },
+    new() { "C#", "Python" },
+    new() { "Python", "HTML" }
+};
+
+List<int> results = new() { 0, 0, 1 };
+
+WriteLine($"Winner: {TournamentWinner(competitions, results)}");
+
+static string TournamentWinner(List<List<string>> competitions, List<int> results)
+{
+    Dictionary<string, int> winners = new();
+    int iCounter = 0;
+
+    results.ForEach(result =>
+    {
+        var currentWinner = (result == 0) ? competitions[iCounter][1] : competitions[iCounter][0];
+        iCounter++;
+
+        if(winners.ContainsKey(currentWinner))
+        {
+            winners[currentWinner]+= 3;
+        }
+        else
+        {
+            winners.Add(currentWinner, 3);
+        }
+        // WriteLine(currentWinner);
+    });
+
+    
+    return winners.OrderByDescending(x => x.Value).First().Key;
+}
+
 static void ShowCaseTryParseEnumDemo()
 {
     ForegroundColor = ConsoleColor.DarkCyan;
