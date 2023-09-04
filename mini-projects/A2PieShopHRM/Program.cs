f26cd232a38f4078fbee25327ad06b8315d58682
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-04 23:02:18 +0530

    Update Program.cs

--- mini-projects/A2PieShopHRM/Program.cs
+++ mini-projects/A2PieShopHRM/Program.cs
@@ -1,4 +1,14 @@
 ﻿WriteLine("Welcome to Pie Shop HRM");
 
+int age = 25;
+string fullName = "Sri Varu";
+bool isManager = true;
+decimal salary = 1250.48M;
+
+WriteLine($"Name: {fullName}");
+WriteLine($"Age: {age}");
+WriteLine($"Manager: {isManager}");
+WriteLine($"Salary: ${salary}");
+
 WriteLine("\n\nThank You. Visit Again!");
 ReadLine();
