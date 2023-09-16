3099c70a858b54f7f72a4388bc89e1479c442d37
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-16 17:18:33 +0530

    Moved the code into Methods

--- 202309-Fundamentals/BethanysPieShopHRM/Program.cs
+++ 202309-Fundamentals/BethanysPieShopHRM/Program.cs
@@ -1,20 +1,31 @@
 ﻿
 WriteLine("Welcome to Bethany's Pie Shop HRM");
 
-WriteLine("Enter your name:");
-string? name = ReadLine();
-
-WriteLine($"Employee Name: {name}");
-
-
-byte numberOfEmployees = 125;
-int months = 12, bonus = 1000;
-var monthlyWage = 1234;
-bool isActive = true;
-double rating = 99.25;
-int hoursWorked = 180;
-const double interestRate = 0.07;
+ShowBasicDataTypesDemo();
 
 WriteLine("\n\nPress any key ...");
 ReadKey();
 
+static void ShowBasicDataTypesDemo()
+{
+    byte numberOfEmployees = 125;
+    int months = 12, bonus = 1000;
+    var monthlyWage = 1234;
+    bool isActive = true;
+    double rating = 99.25;
+    int hoursWorked = 180;
+    const double interestRate = 0.07;
+
+    Write("Enter your name:");
+    string? name = ReadLine();
+
+    WriteLine($"Number of employees: {numberOfEmployees}");
+    WriteLine($"Employee Name: {name}");
+    WriteLine($"Number of Months: {months}");
+    WriteLine($"Bonus: {bonus}");
+    WriteLine($"Monthly Wage: {monthlyWage}");
+    WriteLine($"Is Active: {isActive}");
+    WriteLine($"Rating: {rating}");
+    WriteLine($"Hours Worked: {hoursWorked}");
+    WriteLine($"Interest Rate: {interestRate}");
+}
