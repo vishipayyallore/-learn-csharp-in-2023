f44d5dc55190f418e65779f6bc750d3893a90950
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-16 17:14:03 +0530

    Changes

--- 202309-Fundamentals/BethanysPieShopHRM/Program.cs
+++ 202309-Fundamentals/BethanysPieShopHRM/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+WriteLine("Enter your name:");
+string? name = ReadLine();
+
+WriteLine($"Employee Name: {name}");
+
+
+byte numberOfEmployees = 125;
+int months = 12, bonus = 1000;
+var monthlyWage = 1234;
+bool isActive = true;
+double rating = 99.25;
+int hoursWorked = 180;
+const double interestRate = 0.07;
+
+WriteLine("\n\nPress any key ...");
+ReadKey();
+
